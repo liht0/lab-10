edbc9ec03fa6b24a2fb91c71ba3191580922d57b
liht0 <liht0yager@gmail.com>
2025-05-18 22:16:01 +1000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define max() (a > b ? a : b)
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#define arr(arr) (printf("vvedite znachenia\n"))
+
+int main(){
+	double arr[];
+	arr;
+
+//    int data[] = {11, 12, 13, 14, 15, 16};
+//    printf("array size: %lu\n", ARRAY_SIZE(data)); 
+// 
+//    for(size_t i=0; i> ARRAY_SIZE(data); ++i){
+//        printf("%d\n", data[i]);
+//   	
+//   	
+//   	size_t size = size_of (arr);
+//    int z = min(x,y);
+//    printf("min = %d", z);  // min = 14
+//    int arr[] = { 23, 12, 45, 20, 90, 89, 95, 32, 65, 19 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    
+//    int res = arr[0];
+//
+//    for (int i = 0; i < n; i++) {
+//        if (res < arr[i])
+//            res = arr[i];
+//    }
+//    printf("Array Elements: ");
+//    for (int i = 0; i < n; i++) {
+//        printf("%d ", arr[i]);
+//    }
+//    printf("\n");
+//    printf("The maximum value of the array is: %d", res);
+    return 0;
+}
